ade62c1605281aa64b6ee6c60c4b8429d50da463
MinhQuangMindx <vincentequang97@gmail.com>
2023-07-05 20:28:19 +0700

    Create button.js

---
+++ hello-world/src/components/button.js
@@ -0,0 +1,17 @@
+// import React from "react";
+
+// const Button = ({ type, nameBtn, onclick }) => {
+//   return (
+//     <div class="mb-3">
+//       <button
+//         className="form__btn btn btn-outline-success"
+//         type={type}
+//         onClick={onclick}
+//       >
+//         {nameBtn}
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Button;
