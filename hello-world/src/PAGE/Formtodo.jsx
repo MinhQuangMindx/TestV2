4e9f5e976f91652746cf15776ce8e764c26c40d9
MinhQuangMindx <vincentequang97@gmail.com>
2023-07-05 20:27:53 +0700

    Create Formtodo.jsx

---
+++ hello-world/src/PAGE/Formtodo.jsx
@@ -0,0 +1,45 @@
+// import Button from "../components/button";
+// import CustomInput from "../components/input";
+// import React from "react";
+
+// function Todo() {
+//   const { showNotity, actSubmit, work, onChange } = useHooktodo();
+//   return (
+//     <div className="form_todo">
+//       <span className="title_todo">#todo</span>
+//       <hr></hr>
+//       <CustomInput
+//         className="form__input--All"
+//         type="text"
+//         placeholder="add details"
+//         value={work.FormAll}
+//         onChange={(e) => {
+//           onChange(e, "FormAll");
+//         }}
+//       >
+//         {" "}
+//       </CustomInput>
+//       <Button type="button" nameBtn="Add" onclick={actSubmit}></Button>
+//       {showNotify && (
+//         <div
+//           class="toast align-items-center"
+//           role="alert"
+//           aria-live="assertive"
+//           aria-atomic="true"
+//         >
+//           <div class="d-flex">
+//             <div class="toast-body">Hello, world! This is a toast message.</div>
+//             <button
+//               type="button"
+//               class="btn-close me-2 m-auto"
+//               data-bs-dismiss="toast"
+//               aria-label="Close"
+//             ></button>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Todo;
