5252be84319f99ca8f2d24efc4b56f079f8eb7ba
MinhQuangMindx <vincentequang97@gmail.com>
2023-07-05 20:28:16 +0700

    Create input.js

---
+++ hello-world/src/components/input.js
@@ -0,0 +1,20 @@
+// const CustomInput = ({ title, type, name, placeholder, value, onChange }) => {
+//   return (
+//     <div class="mb-9">
+//       <label for="formGroupExampleInput" class="form-label">
+//         {title}
+//       </label>
+//       <input
+//         type={type}
+//         class="form-control"
+//         placeholder={placeholder}
+//         aria-label={name}
+//         aria-describedby="basic-addon1"
+//         value={value}
+//         onChange={onChange}
+//       />
+//     </div>
+//   );
+// };
+
+// export default CustomInput;
